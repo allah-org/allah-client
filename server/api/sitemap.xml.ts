cdc0ad68d5ed236e6d42fdc79802ffebc1b0d33a
anonymous <anonymous@gmail.com>
2025-08-06 16:46:50 +0800

    [feat] add public sitemap

--- server/api/sitemap.xml.ts
+++ server/api/sitemap.xml.ts
@@ -4,31 +4,24 @@ export default defineEventHandler(async (event) => {
   const baseUrl = 'https://btt.zeusoftech.com'
   const currentDate = new Date().toISOString()
   
+  // 可以從數據庫或文件系統動態獲取頁面
+  const pages = [
+    { path: '/', priority: '1.0', changefreq: 'daily' },
+    { path: '/business', priority: '0.8', changefreq: 'weekly' },
+    { path: '/hr', priority: '0.8', changefreq: 'weekly' },
+    { path: '/contact', priority: '0.6', changefreq: 'monthly' },
+    // 未來可以輕鬆添加更多頁面
+  ]
+  
+  const sitemapUrls = pages.map(page => `
+  <url>
+    <loc>${baseUrl}${page.path}</loc>
+    <lastmod>${currentDate}</lastmod>
+    <changefreq>${page.changefreq}</changefreq>
+    <priority>${page.priority}</priority>
+  </url>`).join('')
+  
   return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${baseUrl}/</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/business</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/hr</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/contact</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.6</priority>
-  </url>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${sitemapUrls}
 </urlset>`
 }) 
